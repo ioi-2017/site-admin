ae0f4541591121ee3888760a226093724b2522d7
Hamed Saleh <hameelas@gmail.com>
2017-02-21 20:48:39 +0330

    added details to visualization models

--- visualization/models.py
+++ visualization/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Room(models.Model):
-    pass
+    name = models.CharField()
 
 
 class Desk(models.Model):
     contestant = models.OneToOneField('Contestant', related_name='desk')
-    node = models.OneToOneField('Node', related_name='desk')
+    active_node = models.OneToOneField('Node', related_name='desk')
+    room = models.ForeignKey('Room', on_delete=models.CASCADE)
 
 
 class Contestant(models.Model):
@@ -17,3 +18,4 @@ class Contestant(models.Model):
 class Node(models.Model):
     mac_address = models.CharField()
     ip = models.IPAddressField()
+    property_id = models.CharField()
