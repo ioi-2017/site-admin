03f5e1cef4f5883c6c1db9a9acd6360bfe472e92
Hamed Saleh <hameelas@gmail.com>
2017-02-21 20:43:01 +0330

    Initialize visualizations model

--- visualization/models.py
+++ visualization/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    pass
+
+
+class Desk(models.Model):
+    contestant = models.OneToOneField('Contestant', related_name='desk')
+    node = models.OneToOneField('Node', related_name='desk')
+
+
+class Contestant(models.Model):
+    name = models.CharField()
+
+
+class Node(models.Model):
+    mac_address = models.CharField()
+    ip = models.IPAddressField()
