bee114ac4a5ac0dd9711e24cc2c14c286a54bde2
Mohammad Amin Khashkhashi Moghaddam <amin.moghaddamv@gmail.com>
2017-02-21 20:56:15 +0330

    Add some models to `task_admin`

--- task_admin/models.py
+++ task_admin/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_celery_results.models import TaskResult
+
 
 # Create your models here.
+class Task(models.Model):
+    is_local = models.BooleanField()
+    name = models.CharField(max_length=100)
+    code = models.TextField()
+    author = models.ForeignKey(User)
+    created_at = models.DateTimeField()
+    deleted = models.BooleanField()
+
+
+class TaskRun(models.Model):
+    celery_task = models.ForeignKey(TaskResult)
+
+    @property
+    def is_done(self):
+        raise NotImplemented()
+
+    @property
+    def duration(self):
+        raise NotImplemented()
+
+    @property
+    def started_at(self):
+        raise NotImplemented()
+
+    @property
+    def created_at(self):
+        raise NotImplemented()
+
+    @property
+    def finished_at(self):
+        raise NotImplemented()
+
+
