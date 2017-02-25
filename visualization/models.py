9f5c52b8ae40649fb28f4731dabd8a7137c033a3
Hamed Saleh <hameelas@gmail.com>
2017-02-25 03:33:59 +0330

    revise the position concept

--- visualization/models.py
+++ visualization/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django_countries.fields import CountryField
 
@@ -13,13 +14,14 @@ class Desk(models.Model):
     contestant = models.OneToOneField('Contestant', related_name='desk')
     active_node = models.OneToOneField('Node', related_name='desk')
     room = models.ForeignKey('Room', on_delete=models.CASCADE)
+    x = models.DecimalField(max_digits=5, decimal_places=5, default=0)
+    y = models.DecimalField(max_digits=5, decimal_places=5, default=0)
+    angle = models.IntegerField(default=0, validators=[
+        MaxValueValidator(359), MinValueValidator(0)
+    ])
 
     def __str__(self):
         return "%s - %s" % (self.contestant.identifier, self.active_node)
-
-
-class Position(models.Model):
-    pass
 
 
 class Contestant(models.Model):
