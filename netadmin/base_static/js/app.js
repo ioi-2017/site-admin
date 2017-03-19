b45351aef4b8dbe805f1d326bfbb0e1c4c5b4678
Hamed Saleh <hameelas@gmail.com>
2017-03-19 15:55:21 +0330

    extracting angular directives (sidebar and header)

--- netadmin/base_static/js/app.js
+++ netadmin/base_static/js/app.js
@@ -5,7 +5,7 @@
 
 var app = angular.module('NetAdmin', ['ngMaterial', 'ngResource']);
 
-app.config(function($mdThemingProvider) {
+app.config(function ($mdThemingProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('grey', {
             'default': '800',
@@ -19,10 +19,34 @@ app.config(function($mdThemingProvider) {
         .dark();
 });
 
+app.directive('sideBar', function () {
+    return {
+        'controllerAs': 'sidebar',
+        'controller': function () {
+            this.contest_logo = _static('images/ioi2017.png');
+            this.contest_title = 'IOI 2017';
+        },
+        'templateUrl': _static('templates/sidebar.html'),
+        'replace': true
+    };
+});
+
 app.controller('NavController', function () {
     this.items = [
-        { 'name': 'Monitor', 'children': ['All'] },
-        { 'name': 'Admin', 'children': ['Task', 'TaskRunSets'] },
-        { 'name': 'Config', 'children': ['Nodes', 'People', 'Desks']}
+        {'name': 'Monitor', 'children': ['All']},
+        {'name': 'Admin', 'children': ['Task', 'TaskRunSets']},
+        {'name': 'Config', 'children': ['Nodes', 'People', 'Desks']}
     ];
+});
+
+app.directive('headBar', function () {
+    return {
+        'controllerAs': 'header',
+        'controller': function () {
+            this.user = 'Hamed';
+            this.location = 'Home';
+        },
+        'templateUrl': _static('templates/header.html'),
+        'replace': true
+    }
 });
