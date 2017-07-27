5be5a6957d4a09ea679be44979dd6d9b65f1df2c
Hamed Saleh <hameelas@gmail.com>
2017-07-27 18:28:28 +0430

    Add useful print to import_dhcp

--- visualization/management/commands/import_dhcp.py
+++ visualization/management/commands/import_dhcp.py
@@ -17,6 +17,8 @@ class Command(BaseCommand):
             mac_address = ':'.join([mac[0:2], mac[2:4], mac[4:6], mac[6:8], mac[8:10], mac[10:12]]).upper()
             try:
                 node = Node.objects.get(mac_address=mac_address)
+                if node.ip != ip:
+                    print('Changed', node.ip, ip)
                 node.ip = ip
                 node.save()
             except Exception:
